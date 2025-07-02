afa1676fed24316b93224739314a8874561440d5
depthstack <rit609009@gmail.com>
2025-07-02 11:35:59 +0530

    Initial Commit

---
+++ Moody/MoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp.swift
+//  Moody
+//
+//  Created by Shazan Zaidi on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
